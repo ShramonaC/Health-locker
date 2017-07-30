0ce5a48720f27793b9c143579193702cb3a7a8f5
ShramonaC <chakrabortyshramona@gmail.com>
2017-07-30 21:46:22 +0530

    First add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,33 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule} from 'angularfire2';
+import * as firebase from 'firebase';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const config = {
+	apiKey: "AIzaSyCJJKmo8jIS0UYE1UEfTJjiCepQeGId6Qg",
+    authDomain: "ionic2-storage-e04a8.firebaseapp.com",
+    databaseURL: "https://ionic2-storage-e04a8.firebaseio.com",
+    projectId: "ionic2-storage-e04a8",
+    storageBucket: "ionic2-storage-e04a8.appspot.com",
+    messagingSenderId: "167314487064"
+};
+ firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+  
+  function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
 
 @NgModule({
   declarations: [
@@ -14,7 +38,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+	AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
